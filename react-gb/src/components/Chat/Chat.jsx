deae67d29725aeab383f0f2f0d14c8a11ee0837e
IvlevaAnna <annaiva19@mail.ru>
2022-02-21 02:08:01 +0300

    [React] Task6

--- react-gb/src/components/Chat/Chat.jsx
+++ react-gb/src/components/Chat/Chat.jsx
@@ -1,56 +1,54 @@
 import React, {useEffect, useState} from 'react'
-import {Navigate, useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import s from "./Chat.module.css";
 import {MessageForm} from "../MessageForm/MessageForm";
 import {Message} from "../Message/Message";
+import {useDispatch, useSelector} from "react-redux";
+import {messagesSelector} from "../../store/messages/selectors";
+import {addMessage, deleteChatMessages} from "../../store/messages/actions";
+import {deleteChat} from "../../store/chats/actions";
+import Button from "@material-ui/core/Button/Button";
 
 export const Chat = () => {
+    const dispatch = useDispatch()
     const {chatId} = useParams()
+    const navigate = useNavigate()
 
-    const [messageList, setMessageList] = useState({
-        chat1: [
-            {text: 'hello', author: 'me', id: `chat1-msg1`},
-            {text: 'hello', author: 'me', id: `chat1-msg2`},
-        ],
-        chat2: [
-            {text: 'Hello, Nick!', author: 'me', id: `chat2-msg1`},
-        ],
-        chat3: [
-            {text: 'Hi, sisters!', author: 'me', id: `chat3-msg1`},
-            {text: 'What are you doin?', author: 'me', id: `chat3-msg2`},
-        ],
-        chat4: [
-            {text: 'Good morning', author: 'me', id: `chat4-msg1`},
-        ],
-    })
+    const messageList = useSelector(messagesSelector)
 
     useEffect(() => {
         let timeout
-        if(messageList[chatId][messageList[chatId]?.length -1].author === 'me') {
+        if(messageList[chatId][messageList[chatId]?.length -1]?.author === 'me') {
             timeout = setTimeout(() => {
-                setMessageList((prevMessageList) =>
-                    ({ ...prevMessageList, [chatId] : [...prevMessageList[chatId], {text: `Your message is: ${prevMessageList[chatId][prevMessageList[chatId].length-1].text}`, author: 'robot', id: `msg-${Date.now()}`}]}))
+                dispatch(addMessage(chatId,
+                    {text: `Your message is: ${messageList[chatId][messageList[chatId].length-1].text}`, author: 'robot', id: `msg-${Date.now()}`}))
             }, 1500)
         }
 
         return () => clearTimeout(timeout)
     }, [messageList])
 
-
     const handleSubmit = (messageText) => {
-        setMessageList((prevMessageList) =>
-            ({...prevMessageList, [chatId] : [...prevMessageList[chatId], {text: messageText, author: 'me', id: `msg-${Date.now()}`}] }))
-
+        dispatch(addMessage(chatId, {text: messageText, author: 'me', id: `msg-${Date.now()}`}))
     }
 
     if (!messageList[chatId]) {
-        return <Navigate to='/chats' replace />
+        return navigate('../chats', {replace: true})
+    }
+
+    const handleDelete = () => {
+        dispatch(deleteChatMessages(chatId))
+        dispatch(deleteChat(chatId))
+        navigate('/chats', {replace: true})
     }
 
     return (
         <div className={s.container}>
+            <Button aria-label="delete" onClick={handleDelete}>
+                delete chat
+            </Button>
             {
-                messageList[chatId].map((message) =>
+                messageList[chatId]?.map((message) =>
                     <Message text={message.text} author={message.author} key={message.id}/>)
             }
             <MessageForm onSubmit={handleSubmit}/>
--- react-gb/src/views/Chats/Chats.jsx
+++ react-gb/src/views/Chats/Chats.jsx
@@ -1,16 +1,40 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import s from './Chats.module.css'
 import List from "@material-ui/core/List/List";
 import {Link, Outlet } from "react-router-dom";
 import ListItem from "@material-ui/core/ListItem/ListItem";
+import {useDispatch, useSelector} from "react-redux";
+import {addChat, deleteChat} from "../../store/chats/actions";
+import {chatsSelector} from "../../store/chats/selectors";
+import Button from "@material-ui/core/Button/Button";
+import {Dialog, DialogContent, Modal, TextField} from "@material-ui/core";
+import {addChatMessages} from "../../store/messages/actions";
 
 export const Chats = () => {
-    const chats =[
-        {id: 'chat1', name: 'Friends'},
-        {id: 'chat2', name: 'Fam'},
-        {id: 'chat3', name: 'Besties'},
-        {id: 'chat4', name: 'Nick'},
-    ]
+    const dispatch = useDispatch()
+    const [open, setOpen] = useState(false)
+    const [name, setName] = useState('')
+
+    const chats = useSelector(chatsSelector)
+
+    const handleOpen = () => {
+        setOpen(true)
+    }
+
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        dispatch(addChat(`chat-${name}`, name))
+        dispatch(addChatMessages(`chat-${name}`))
+        setOpen(false)
+    }
 
     return (
         <div className={s.container}>
@@ -23,6 +47,16 @@ export const Chats = () => {
                             </ListItem>)
                     }
                 </List>
+                <Button onClick={handleOpen}>add chat</Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogContent>
+                        <TextField value={name} onChange={handleChange}/>
+                        <Button onClick={handleSubmit}>add chat</Button>
+                    </DialogContent>
+                </Dialog>
             </div>
             <Outlet/>
         </div>
